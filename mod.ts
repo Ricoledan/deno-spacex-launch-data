4a86a741db29722b6325098d58bb4f93aa5e03d3
Ricardo <ricoledan@gmail.com>
2020-10-21 20:23:00 -0400

    add rocket & customers to interface, add logic to pull data

--- mod.ts
+++ mod.ts
@@ -3,6 +3,8 @@ import * as log from "https://deno.land/std/log/mod.ts";
 interface Launch {
   flightNumber: number;
   mission: string;
+  rocket: any;
+  customers: Array<string>;
 }
 
 const launches = new Map<number, Launch>();
@@ -16,6 +18,32 @@ export async function downloadLaunchData() {
     },
   );
 
+  const getRocketName = async (id: string) => {
+    const response = await fetch(
+      `https://api.spacexdata.com/v4/rockets/${id}`,
+      {
+        method: "GET",
+      },
+    );
+
+    const result = await response.json();
+    return result["name"];
+  };
+
+  const getCustomerPayload = async (id: string) => {
+    const response = await fetch(
+      `https://api.spacexdata.com/v4/payloads/${id}`,
+      {
+        method: "GET",
+      },
+    );
+
+    const result = await response.json();
+    return result["customers"];
+  };
+
+  getCustomerPayload("5eb0e4b5b6c3bb0006eeb1e1");
+
   if (!response.ok) {
     log.warning("Problem downloading launch data.");
     throw new Error("Launch data download failed.");
@@ -26,6 +54,9 @@ export async function downloadLaunchData() {
     const flightData = {
       flightNumber: launch["flight_number"],
       mission: launch["name"],
+      rocket: await getRocketName(launch["rocket"]),
+      // TODO: update to pull in payloads that exceed 1
+      customers: await getCustomerPayload(launch["payloads"][0]),
     };
 
     launches.set(flightData.flightNumber, flightData);
